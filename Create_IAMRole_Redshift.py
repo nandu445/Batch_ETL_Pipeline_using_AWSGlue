1f3f49ff79cdc8330e8410025ff7dfca1e60cfb5
KRISHNA PRIYA MOLAKALAPALLI <84462660+KrishnaPriyaMolakalapalli@users.noreply.github.com>
2024-08-11 18:24:21 -0500

    Update Create_IAMRole_Redshift.py

--- Create_IAMRole_Redshift.py
+++ Create_IAMRole_Redshift.py
@@ -4,7 +4,7 @@ import boto3
 iam_client = boto3.client('iam')
 
 # Define the role name
-role_name = 'Glue_Sales_ETL_Role'
+role_name = 'Redshift_Ecomerce_Role'
 
 # Define the assume role policy that allows Glue to assume this role
 assume_role_policy_document = {
@@ -13,7 +13,7 @@ assume_role_policy_document = {
         {
             "Effect": "Allow",
             "Principal": {
-                "Service": "glue.amazonaws.com"
+                "Service": "redshift.amazonaws.com"
             },
             "Action": "sts:AssumeRole"
         }
@@ -24,95 +24,10 @@ assume_role_policy_document = {
 role_response = iam_client.create_role(
     RoleName=role_name,
     AssumeRolePolicyDocument=json.dumps(assume_role_policy_document),
-    Description='Role to allow Glue to access S3, Glue Data Catalog, and Redshift'
+    Description='Redshift Role'
 )
 
-# Define the S3 policy to allow access to the specific S3 bucket
-s3_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:ListBucket"
-            ],
-            "Resource": [
-                "arn:aws:s3:::ecommerce-sales-etl-bucket",
-                "arn:aws:s3:::ecommerce-sales-etl-bucket/*"
-            ]
-        }
-    ]
-}
-
-# Create and attach the S3 policy
-s3_policy_response = iam_client.put_role_policy(
-    RoleName=role_name,
-    PolicyName='S3AccessPolicy',
-    PolicyDocument=json.dumps(s3_policy)
+iam_client.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn="arn:aws:iam::aws:policy/AmazonRedshiftFullAccess"
 )
-
-# Define the Glue policy to allow access to the Glue Data Catalog
-glue_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "glue:*"
-            ],
-            "Resource": [
-                "arn:aws:glue:*:024848463817:catalog",
-                "arn:aws:glue:*:024848463817:database/ecommerce_sales_database",
-                "arn:aws:glue:*:024848463817:table/ecommerce_sales_database/*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "glue:GetConnection"
-            ],
-            "Resource": [
-                "arn:aws:glue:us-east-2:024848463817:connection/RedshiftEcommerceConnection"
-            ]
-        }
-    ]
-}
-
-# Create and attach the Glue policy
-glue_policy_response = iam_client.put_role_policy(
-    RoleName=role_name,
-    PolicyName='GlueDataCatalogPolicy',
-    PolicyDocument=json.dumps(glue_policy)
-)
-
-# Define the Redshift policy to allow access to the specific Redshift cluster
-redshift_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "redshift:DescribeClusters",
-                "redshift-data:ExecuteStatement",
-                "redshift-data:DescribeStatement",
-                "redshift-data:GetStatementResult",
-                "redshift:GetClusterCredentials"
-            ],
-            "Resource": [
-                "arn:aws:redshift:us-east-2:024848463817:namespace:bf960ccc-2d98-43c8-89da-1c542a197b98",
-                "arn:aws:redshift:us-east-2:024848463817:namespace:bf960ccc-2d98-43c8-89da-1c542a197b98/*"
-            ]
-        }
-    ]
-}
-
-# Create and attach the Redshift policy
-redshift_policy_response = iam_client.put_role_policy(
-    RoleName=role_name,
-    PolicyName='RedshiftAccessPolicy',
-    PolicyDocument=json.dumps(redshift_policy)
-)
-
-print("IAM role created with restricted policies attached.")
